5533789bfc7d5ebe8f36c07d7cbfcaca3dcb1a92
eiiko6 <mxstotolatomate@gmail.com>
2024-08-21 13:47:31 +0200

    fish functions, waybar changes, ..

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -81,6 +81,22 @@ function pys
     nvim +2 $path/src/main.py
 end
 
+# Helper tot trim videos with ffmpeg
+function trim
+    if test (count $argv) -lt 5
+        echo "Usage: trim <input_video> <start_time> <end_time> <bitrate> <output_video>"
+        return 1
+    end
+
+    set input_video $argv[1]
+    set start_time $argv[2]
+    set end_time $argv[3]
+    set bitrate $argv[4]
+    set output_video $argv[5]
+
+    ffmpeg -i $input_video -ss $start_time -to $end_time -c:v libx264 -c:a aac -strict experimental -b:a $bitrate $output_video
+end
+
 
 # ===> Custom aliases
 alias grep='grep --color=auto'
