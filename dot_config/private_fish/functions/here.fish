706d60248603ea3e8267717a0348932dc0b6aa89
eiiko6 <mxstotolatomate@gmail.com>
2024-10-16 19:25:26 +0200

    configured media binds, minor changes to fish functions and themes

--- dot_config/private_fish/functions/here.fish
+++ dot_config/private_fish/functions/here.fish
@@ -1,15 +1,15 @@
 # Display items taking up space in specified directory
 function here
     if test (count $argv) -eq 0
-        du -cha --max-depth=1 ./ | grep -E "M|G"
+        du -cha --max-depth=1 ./ | grep -E "M|G" | sort -rh | grep -v total
     else
-        du -cha --max-depth=1 $argv[1] | grep -E "M|G"
+        du -cha --max-depth=1 $argv[1] | grep -E "M|G" | sort -rh | grep -v total
     end
 end
 function shere
     if test (count $argv) -eq 0
-        sudo du -cha --max-depth=1 ./ | grep -E "M|G"
+        sudo du -cha --max-depth=1 ./ | grep -E "M|G" | sort -rh | grep -v total
     else
-        sudo du -cha --max-depth=1 $argv[1] | grep -E "M|G"
+        sudo du -cha --max-depth=1 $argv[1] | grep -E "M|G" | sort -rh | grep -v total
     end
 end
--- dot_config/private_fish/functions/jvstart.fish
+++ dot_config/private_fish/functions/jvstart.fish
@@ -1,7 +1,11 @@
 # Quickly start a java project
 function jvstart
-    set path (realpath ~/Desktop/random-code/java-projects/)/$argv[1]
+    if test (count $argv) -eq 0
+        echo "No path specified."
+        return
+    end
+    set path (realpath $(pwd))/$argv[1]
     cp -r ~/Desktop/random-code/java-projects/template $path
     cd $path/src
-    nvim +3 $path/src/Main.java
+    cls
 end
