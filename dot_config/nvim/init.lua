fcd89e41e4aa15cc075acf0e59d738aa88dada05
eiiko6 <myeiiko6@gmail.com>
2026-03-29 15:24:27 +0200

    nvim: indent width = 4 please

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -9,9 +9,9 @@ vim.opt.tabstop = 2
 -- Disable line wrap
 vim.o.wrap = false
 
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.autoindent = true
 vim.opt.smartindent = true
