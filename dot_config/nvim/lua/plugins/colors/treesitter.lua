675406c4e9b81e43ecc42f6338b752b6c385fc6a
eiiko6 <myeiiko6@gmail.com>
2026-05-15 23:16:01 +0200

    nvim: switched to tsmanager

--- dot_config/nvim/lua/plugins/colors/treesitter.lua
+++ dot_config/nvim/lua/plugins/colors/treesitter.lua
@@ -1,17 +1,16 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  event = { 'BufReadPre', 'BufNewFile' },
-  build = ':TSUpdate',
-  main = 'nvim-treesitter.configs', -- Sets main module to use for opts
-  opts = {
-    -- ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
-    auto_install = true,
-    highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = { 'ruby' },
-    },
-    indent = { enable = true, disable = { 'ruby' } },
-  },
-  --    - Show current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-  --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+  'romus204/tree-sitter-manager.nvim',
+  dependencies = {}, -- tree-sitter CLI must be installed system-wide
+  config = function()
+    require('tree-sitter-manager').setup {
+      -- Default Options
+      -- ensure_installed = {}, -- list of parsers to install at the start of a neovim session
+      -- border = nil, -- border style for the window (e.g. "rounded", "single"), if nil, use the default border style defined by 'vim.o.winborder'. See :h 'winborder' for more info.
+      auto_install = true, -- if enabled, install missing parsers when editing a new file
+      highlight = true, -- treesitter highlighting is enabled by default
+      -- languages = {}, -- override or add new parser sources
+      -- parser_dir = vim.fn.stdpath("data") .. "/site/parser",
+      -- query_dir = vim.fn.stdpath("data") .. "/site/queries",
+    }
+  end,
 }
