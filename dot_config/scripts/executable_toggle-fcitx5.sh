53a7dcf94881d8013fb5d8cb324ba4bc68cf1462
eiiko6 <myeiiko6@gmail.com>
2026-05-30 12:59:25 +0200

    hyprland portal fixes

--- dot_config/scripts/executable_toggle-fcitx5.sh
+++ dot_config/scripts/executable_toggle-fcitx5.sh
@@ -2,8 +2,10 @@
 FCITX5=$(pidof fcitx5)
 if [ -z "$FCITX5" ]; then
     nohup fcitx5 --disable=wayland_diagnose &
+    notify-send "Started fcitx5"
     exit
 else
     pkill fcitx5
+    notify-send "Killed fcitx5"
     exit
 fi
