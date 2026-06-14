2273e0668e1c3b4fc2d3de11c828fbe982a2ef89
eiiko6 <myeiiko6@gmail.com>
2026-06-14 12:29:31 +0200

    added portal exec on start

--- dot_config/hypr/hyprland.lua
+++ dot_config/hypr/hyprland.lua
@@ -18,6 +18,7 @@ emojiPickerScript = "~/.config/scripts/emoji-picker.sh"
 hl.on("hyprland.start", function()
 	hl.exec_cmd("dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
 	hl.exec_cmd("systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
+	hl.exec_cmd("~/.config/scripts/start-portal.sh")
 	-- hl.exec_cmd("/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1")
 	hl.exec_cmd("gsettings set org.gnome.desktop.interface cursor-theme 'BreezeX-RosePine-Linux'")
 	hl.exec_cmd("awww-daemon")
