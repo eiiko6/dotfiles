0712f5fe79e6ece310bc581fd81c59fde2ac624d
eiiko6 <myeiiko6@gmail.com>
2026-07-02 21:36:16 +0200

    updated script to use self hosted gitea instead of old github

--- installer.sh
+++ installer.sh
@@ -99,159 +99,159 @@ sudo-rs
 
 # Function to detect the Linux distribution
 detect_distro() {
-  if [[ -f /etc/os-release ]]; then
-    . /etc/os-release
+    if [[ -f /etc/os-release ]]; then
+        . /etc/os-release
 
-    if [[ "$ID" == "arch" ]] || [[ "$ID_LIKE" == *"arch"* ]]; then
-      DISTRO="arch"
+        if [[ "$ID" == "arch" ]] || [[ "$ID_LIKE" == *"arch"* ]]; then
+            DISTRO="arch"
+        else
+            DISTRO="unsupported"
+        fi
     else
-      DISTRO="unsupported"
+        DISTRO="unsupported"
     fi
-  else
-    DISTRO="unsupported"
-  fi
 }
 
 # Ask for install type: default or extended
 choose_install_type() {
-  echo "Select install type:"
-  echo "1) Default (minimal setup)"
-  echo "2) Extended (includes Firefox, Vesktop, Spotify, etc...)"
-  read -rp "Enter 1 or 2: " install_choice
+    echo "Select install type:"
+    echo "1) Default (minimal setup)"
+    echo "2) Extended (includes Firefox, Vesktop, Spotify, etc...)"
+    read -rp "Enter 1 or 2: " install_choice
 
-  case "$install_choice" in
-  2)
-    INSTALL_EXTENDED=true
-    ;;
-  *)
-    INSTALL_EXTENDED=false
-    ;;
-  esac
+    case "$install_choice" in
+        2)
+            INSTALL_EXTENDED=true
+            ;;
+        *)
+            INSTALL_EXTENDED=false
+            ;;
+    esac
 }
 
 # Ask if the user wants to run the oxidation process
 ask_oxidize() {
-  read -rp "Do you want to oxidize the install (Rust tools and tweaks)? [y/N] " oxidize
-  case "$oxidize" in
-  [Yy]*) OXIDIZE_INSTALL=true ;;
-  *) OXIDIZE_INSTALL=false ;;
-  esac
+    read -rp "Do you want to oxidize the install (Rust tools and tweaks)? [y/N] " oxidize
+    case "$oxidize" in
+        [Yy]*) OXIDIZE_INSTALL=true ;;
+        *) OXIDIZE_INSTALL=false ;;
+    esac
 }
 
 oxidize_install() {
-  echo "Installing Rust-powered replacements..."
+    echo "Installing Rust-powered replacements..."
 
-  paru -S --noconfirm --needed --sudoloop $RUST_PACKAGES
+    paru -S --noconfirm --needed --sudoloop $RUST_PACKAGES
 
-  rustup default stable
+    rustup default stable
 
-  # Create overrides directory
-  mkdir -p ~/.local/rust-overrides
+    # Create overrides directory
+    mkdir -p ~/.local/rust-overrides
 
-  # Add to private fish config
-  mkdir -p ~/private
-  {
-    echo 'set -U fish_user_paths ~/.local/rust-overrides $fish_user_paths'
-    echo 'zoxide init fish | source'
-  } >>~/private/config.fish
+    # Add to private fish config
+    mkdir -p ~/private
+    {
+        echo 'set -U fish_user_paths ~/.local/rust-overrides $fish_user_paths'
+        echo 'zoxide init fish | source'
+    } >>~/private/config.fish
 
-  # Link uu-* binaries
-  for bin in /usr/bin/uu-*; do
-    base_name=$(basename "$bin" | sed 's/^uu-//')
-    ln -sf "$bin" "$HOME/.local/rust-overrides/$base_name"
-  done
+    # Link uu-* binaries
+    for bin in /usr/bin/uu-*; do
+        base_name=$(basename "$bin" | sed 's/^uu-//')
+        ln -sf "$bin" "$HOME/.local/rust-overrides/$base_name"
+    done
 
-  # Link sudo-rs and su-rs
-  ln -sf /usr/bin/sudo-rs "$HOME/.local/rust-overrides/sudo"
-  ln -sf /usr/bin/su-rs "$HOME/.local/rust-overrides/su"
+    # Link sudo-rs and su-rs
+    ln -sf /usr/bin/sudo-rs "$HOME/.local/rust-overrides/sudo"
+    ln -sf /usr/bin/su-rs "$HOME/.local/rust-overrides/su"
 
-  # Install some additional tools
-  cargo install --git "https://github.com/eiiko6/here"
-  rm ~/.config/fish/functions/here.fish
+    # Install some additional tools
+    cargo install --git "https://git.alatreon.org/strawberries/here"
+    rm ~/.config/fish/functions/here.fish
 
-  echo "Rust-based tools installed and configured."
+    echo "Rust-based tools installed and configured."
 }
 
 # Function to get sudo credentials (only ask for password once)
 validate_sudo() {
-  if [[ $EUID -ne 0 ]]; then
-    echo "Please enter your password to proceed with the installation."
-    sudo -v
-  fi
+    if [[ $EUID -ne 0 ]]; then
+        echo "Please enter your password to proceed with the installation."
+        sudo -v
+    fi
 }
 
 # Handle Arch
 handle_arch() {
-  echo "Detected Arch-based system."
+    echo "Detected Arch-based system."
 
-  echo "Installing required packages..."
-  sudo pacman -Syu --noconfirm || {
-    echo "Failed to execute pacman"
-    return 1
-  }
-  sudo pacman -S --needed --noconfirm $MAIN_PACKAGES
+    echo "Installing required packages..."
+    sudo pacman -Syu --noconfirm || {
+        echo "Failed to execute pacman"
+        return 1
+    }
+    sudo pacman -S --needed --noconfirm $MAIN_PACKAGES
 
-  echo "Packages installation ended"
-  echo "Now installing dotfiles"
+    echo "Packages installation ended"
+    echo "Now installing dotfiles"
 
-  # Install dotfiles with chezmoi
-  chezmoi init --apply https://github.com/eiiko6/dotfiles.git
-  echo "chezmoi installed and initialized with your dotfiles."
+    # Install dotfiles with chezmoi
+    chezmoi init --apply https://git.alatreon.org/strawberries/dotfiles.git
+    echo "chezmoi installed and initialized with your dotfiles."
 
-  # Replace my username with current user
-  find ~/.config -type f -exec sed -i "s/strawberries/$(whoami)/g" {} +
+    # Replace my username with current user
+    find ~/.config -type f -exec sed -i "s/strawberries/$(whoami)/g" {} +
 
-  rustup default stable
+    rustup default stable
 
-  # Install my profile switcher
-  cargo install --git "https://github.com/eiiko6/dotswitch"
+    # Install my profile switcher
+    cargo install --git "https://git.alatreon.org/strawberries/dotswitch"
 
-  echo "Dotfiles installation ended"
-  echo "Now installing paru"
+    echo "Dotfiles installation ended"
+    echo "Now installing paru"
 
-  # Install paru
-  if ! command -v paru &>/dev/null; then
-    echo "paru not found. Installing..."
-    sudo pacman -S --needed --noconfirm git base-devel
-    sudo pacman -S --needed base-devel
-    git clone https://aur.archlinux.org/paru.git /tmp/paru
-    cd /tmp/paru
-    makepkg -si
-    cd && rm -rf /tmp/paru
-  else
-    echo "paru is already installed."
-  fi
+    # Install paru
+    if ! command -v paru &>/dev/null; then
+        echo "paru not found. Installing..."
+        sudo pacman -S --needed --noconfirm git base-devel
+        sudo pacman -S --needed base-devel
+        git clone https://aur.archlinux.org/paru.git /tmp/paru
+        cd /tmp/paru
+        makepkg -si
+        cd && rm -rf /tmp/paru
+    else
+        echo "paru is already installed."
+    fi
 
-  echo "paru installation ended"
+    echo "paru installation ended"
 
-  # Install paru packages
-  paru -S --noconfirm --needed --sudoloop $AUR_PACKAGES
+    # Install paru packages
+    paru -S --noconfirm --needed --sudoloop $AUR_PACKAGES
 
-  # Set color themes with default temporary wallpaper
-  fish -c 'source ~/.config/fish/config.fish; theme macchiato'
-  mkdir -p "$HOME/Pictures/Wallpapers"
-  cp "$(find "$HOME"/.config/dotswitch/themes/ -type f -name 'wallpaper.png' | shuf -n 1)" "$HOME/Pictures/Wallpapers/wallpaper.png"
-  # ~/.config/scripts/palette/change-wallpaper.sh wallpaper
+    # Set color themes with default temporary wallpaper
+    fish -c 'source ~/.config/fish/config.fish; theme macchiato'
+    mkdir -p "$HOME/Pictures/Wallpapers"
+    cp "$(find "$HOME"/.config/dotswitch/themes/ -type f -name 'wallpaper.png' | shuf -n 1)" "$HOME/Pictures/Wallpapers/wallpaper.png"
+    # ~/.config/scripts/palette/change-wallpaper.sh wallpaper
 
-  # Setup directories
-  mkdir -p ~/.local/share/icons ~/Desktop/
+    # Setup directories
+    mkdir -p ~/.local/share/icons ~/Desktop/
 
-  # sudo mv /usr/share/icons/rose-pine-hyprcursor/ ~/.local/share/icons/
-  cp -r /usr/share/icons/rose-pine-hyprcursor/ ~/.local/share/icons/
+    # sudo mv /usr/share/icons/rose-pine-hyprcursor/ ~/.local/share/icons/
+    cp -r /usr/share/icons/rose-pine-hyprcursor/ ~/.local/share/icons/
 }
 
 install_extra_packages() {
-  printf "Installing $(echo $EXTRA_PACKAGES | tr ' ' '\n' | wc -l) extra packages"
+    printf "Installing $(echo $EXTRA_PACKAGES | tr ' ' '\n' | wc -l) extra packages"
 
-  # Install the optional packages
-  paru -S --noconfirm --needed --sudoloop $EXTRA_PACKAGES
+    # Install the optional packages
+    paru -S --noconfirm --needed --sudoloop $EXTRA_PACKAGES
 }
 
 # Handle unsupported distros
 handle_unsupported() {
-  echo "Unsupported Linux distribution: $DISTRO"
-  echo "Currently, only Arch is supported."
-  exit
+    echo "Unsupported Linux distribution: $DISTRO"
+    echo "Currently, only Arch is supported."
+    exit
 }
 
 # --------- EXECUTION ----------
@@ -268,20 +268,20 @@ validate_sudo
 
 # Main script execution
 case $DISTRO in
-arch)
-  handle_arch
+    arch)
+        handle_arch
 
-  if $INSTALL_EXTENDED; then
-    install_extra_packages
-  fi
+        if $INSTALL_EXTENDED; then
+            install_extra_packages
+        fi
 
-  if $OXIDIZE_INSTALL; then
-    oxidize_install
-  fi
+        if $OXIDIZE_INSTALL; then
+            oxidize_install
+        fi
 
-  echo "==> Dotfiles installation has ended."
-  ;;
-*)
-  handle_unsupported
-  ;;
+        echo "==> Dotfiles installation has ended."
+        ;;
+    *)
+        handle_unsupported
+        ;;
 esac
