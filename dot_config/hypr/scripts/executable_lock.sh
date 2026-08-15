de7f399d9a0abd721441cc3fce0caf474ef7498e
eiiko6 <myeiiko6@gmail.com>
2026-08-15 17:20:55 +0200

    split noctalia config to isolate appearance, and added macchiato-focus noctalia config

--- dot_config/hypr/scripts/executable_lock.sh
+++ dot_config/hypr/scripts/executable_lock.sh
@@ -1,3 +1,4 @@
-geometry=$(hyprctl monitors | sed -n '2p' | cut -d '@' -f 1 | xargs)
-grim -g "0,0 $geometry" "/tmp/lock-background.png"
-pidof hyprlock || hyprlock
+# geometry=$(hyprctl monitors | sed -n '2p' | cut -d '@' -f 1 | xargs)
+# grim -g "0,0 $geometry" "/tmp/lock-background.png"
+# pidof hyprlock || hyprlock
+noctalia msg session lock
