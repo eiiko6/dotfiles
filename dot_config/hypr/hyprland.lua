2c6257197b9c474b504910ae41703b3d44351a58
eiiko6 <myeiiko6@gmail.com>
2026-09-24 21:57:42 +0200

    hyprland config adjustment

--- dot_config/hypr/hyprland.lua
+++ dot_config/hypr/hyprland.lua
@@ -2,7 +2,9 @@
 mainMod = "SUPER"
 terminal = "kitty -o allow_remote_control=yes -o enabled_layouts=tall"
 secondaryTerminal = "st"
-browser = "env MOZ_ENABLE_WAYLAND=1 firefox"
+-- browser = "env MOZ_ENABLE_WAYLAND=1 firefox"
+-- browser = "/home/strawberries/.local/bin/zen"
+browser = "librewolf"
 -- fileManager = "thunar"
 fileManager = "nautilus -w"
 
@@ -17,68 +19,66 @@ emojiPickerScript = "~/.config/scripts/emoji-picker.sh"
 
 -- Execute background apps at launch
 hl.on("hyprland.start", function()
-    hl.exec_cmd("dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
-    hl.exec_cmd("systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
-    hl.exec_cmd("~/.config/scripts/start-portal.sh")
-    -- hl.exec_cmd("/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1")
-    hl.exec_cmd("gsettings set org.gnome.desktop.interface cursor-theme 'BreezeX-RosePine-Linux'")
-    -- hl.exec_cmd("awww-daemon")
-    hl.exec_cmd("thunar --daemon")
-    -- hl.exec_cmd("waybar")
-    -- hl.exec_cmd("hypridle")
-    -- hl.exec_cmd("mako")
-    -- hl.exec_cmd("wireplumber")
-    -- hl.exec_cmd("blueman-applet")
-    -- hl.exec_cmd("nm-applet --indicator")
-    hl.exec_cmd("wl-paste --watch cliphist store")
-    -- hl.exec_cmd("~/.config/waybar/scripts/album_art.sh")
-    -- hl.exec_cmd("~/Desktop/scripts/wayBarIconAnimation.sh")
-    hl.exec_cmd(palette .. " --all")
-    hl.exec_cmd("noctalia")
+	hl.exec_cmd("dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
+	hl.exec_cmd("systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
+	hl.exec_cmd("~/.config/scripts/start-portal.sh")
+	-- hl.exec_cmd("/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1")
+	hl.exec_cmd("gsettings set org.gnome.desktop.interface cursor-theme 'BreezeX-RosePine-Linux'")
+	-- hl.exec_cmd("awww-daemon")
+	hl.exec_cmd("thunar --daemon")
+	-- hl.exec_cmd("waybar")
+	-- hl.exec_cmd("hypridle")
+	-- hl.exec_cmd("mako")
+	-- hl.exec_cmd("wireplumber")
+	-- hl.exec_cmd("blueman-applet")
+	-- hl.exec_cmd("nm-applet --indicator")
+	hl.exec_cmd("wl-paste --watch cliphist store")
+	-- hl.exec_cmd("~/.config/waybar/scripts/album_art.sh")
+	-- hl.exec_cmd("~/Desktop/scripts/wayBarIconAnimation.sh")
+	hl.exec_cmd(palette .. " --all")
+	hl.exec_cmd("noctalia")
 end)
 
 -- Behavior and Settings
 hl.config({
-    input = {
-        kb_layout = "us,fr",
-        kb_options = "grp:caps_toggle",
-        repeat_delay = 300,
-        repeat_rate = 50,
-        -- kb_options = "caps:none",
-        follow_mouse = 1,
-        touchpad = {
-            natural_scroll = true,
-        },
-        sensitivity = 0, -- -1.0 - 1.0, 0 means no modification.
-        -- accel_profile = "flat"
-    },
-    cursor = {
-        inactive_timeout = 2,
-    },
-    -- dwindle = {
-    --     pseudotile = true, -- Bound to mainMod + P
-    --     preserve_split = true,
-    -- },
-    master = {
-        new_status = "master",
-    },
-    debug = {
-        -- overlay = true
-    },
+	input = {
+		kb_layout = "us,fr",
+		kb_options = "grp:caps_toggle",
+		repeat_delay = 300,
+		repeat_rate = 50,
+		-- kb_options = "caps:none",
+		follow_mouse = 1,
+		touchpad = {
+			natural_scroll = true,
+		},
+		sensitivity = 0, -- -1.0 - 1.0, 0 means no modification.
+		-- accel_profile = "flat"
+	},
+	cursor = {
+		inactive_timeout = 2,
+	},
+	-- dwindle = {
+	--     pseudotile = true, -- Bound to mainMod + P
+	--     preserve_split = true,
+	-- },
+	master = {
+		new_status = "master",
+	},
+	debug = {
+		-- overlay = true
+	},
 
-    misc = {
-        initial_workspace_tracking = 0,
-    },
+	misc = {
+		initial_workspace_tracking = 0,
+	},
 
-    dwindle = {
-        preserve_split = true,
-    },
+	dwindle = {
+		preserve_split = true,
+	},
 })
 
 -- Source config files using loadfile
 local config_dir = os.getenv("HOME") .. "/.config/hypr/"
-
--- require("noctalia").apply_theme()
 
 require("keymaps")
 require("rules")
